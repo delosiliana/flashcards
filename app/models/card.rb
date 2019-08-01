f65f727a9fd238d7ee2b3db073c3b0f7d722f832
delosiliana <dronixa@mail.ru>
2019-08-01 16:23:22 +0300

    add validate text

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
   validates :original_text, :translated_text, :review_date, presence: true
+  validate :check_text
 
   before_validation :set_review_date, only: :create
 
@@ -9,5 +10,12 @@ class Card < ApplicationRecord
 
   def set_review_date
     self.review_date = Time.now + 3.days
+  end
+
+  def check_text
+    if self.original_text.downcase != self.translated_text.downcase
+    else
+      errors.add(:translated_text, 'Должна быть разница между оригинальным и переведенным текстом')
+    end
   end
 end
