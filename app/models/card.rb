5de68ee70a87f0575afe073e75c5c2e75cf6c6ce
delosiliana <dronixa@mail.ru>
2019-08-01 16:44:31 +0300

    fix method

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,7 +13,7 @@ class Card < ApplicationRecord
   end
 
   def check_text
-    if original_text.downcase == translated_text.downcase
+    if original_text.casecmp?(translated_text)
       errors.add(:translated_text, 'Должна быть разница между оригинальным и переведенным текстом')
     end
   end
