8f408fc06aa92bebd6f9fb9300541ed6de42c7bf
delosiliana <dronixa@mail.ru>
2019-08-01 15:00:36 +0300

    add CRUD cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,48 @@
 class CardsController < ApplicationController
+  before_action :set_card, only: [:edit, :update, :destroy]
+
   def index
     @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to cards_path, notice: 'Карточка создана'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @card.update
+      redirect_to cards_path, notice: 'Карточка изменена'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @card.destroy
+      redirect_to cards_path, notice: 'Карточка удалена'
+    else
+      redirect_to cards_path, notice: 'Произошла ошибка'
+    end
+  end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
