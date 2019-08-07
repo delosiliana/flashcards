ea9304e50c3bf5397bcb17ca19eb438dc8c254f3
delosiliana <dronixa@mail.ru>
2019-08-07 19:03:44 +0300

    del folder test

--- test/controllers/flashcards_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FlashcardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get flashcards_index_url
-    assert_response :success
-  end
-
-end
