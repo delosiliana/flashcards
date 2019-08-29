50eafff848c3e19d6168a74d7bac32c3fb2ed447
delosiliana <dronixa@mail.ru>
2019-08-29 14:29:26 +0300

    add test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
+  sequence(:email) { |n| 'test#{n}@test.com'}
+  sequence(:password) { |n| 'password#{n}'}
+  sequence(:password_confirmation) { |n| 'password#{n}'}
+  sequence(:salt) { |n| 'salt#{n}'}
+
   factory :user do
-    
+    email
+    password
+    password_confirmation
+    salt
   end
 end
--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Checking translation' do
+  let!(:password) { 'password' }
+  let(:user) { create(:user, password: password, password_confirmation: password) }
   let(:original_text) { 'Home' }
-  let!(:card) { create :card, original_text: original_text, review_date: Date.today }
+  let!(:card) { create :card, original_text: original_text, review_date: Date.today, user: user }
 
-  before { visit root_path }
+  before(:each) do
+    visit login_url
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: password
+    click_button 'Вход'
+
+    visit root_path
+  end
 
   context 'fill valide answer' do
     it 'should show success alert' do
