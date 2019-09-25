1ab915a08578abbf863e183ba91d0245a0e59272
delosiliana <dronixa@mail.ru>
2019-09-25 16:49:54 +0300

    remove comment

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,6 +1,5 @@
 class FlashcardsController < ApplicationController
   def index
-    #@card = Card.dated.sort_random.last
     @card = if current_user && current_user.decks.find_current_deck.nil?
       Card.dated.sort_random.last
     elsif current_user && !current_user.decks.find_current_deck.nil?
